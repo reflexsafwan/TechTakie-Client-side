e79288cc2f3e1458a3b372084309ee5f195bd201
reflexsafwan <reflex.safwan@gmail.com>
2025-10-02 14:11:56 +0600

    button disabled when loading functionality adde in the sign in button

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -222,10 +222,13 @@ const Login = () => {
             <div className="mt-6">
               <button
                 type="submit"
+                  disabled={loading}
                 className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
               >
-                Sign In
+              {loading?(<span classNameName="loading loading-spinner loading-xs"></span>):"Sign In"}
               </button>
+             
+            
             </div>
           </form>
 
